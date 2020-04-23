a285a47aea37808e2604cc2bc29be03ef7f62f38
Daniel Miller <17dmiller2400@gmail.com>
2020-04-23 00:45:00 -0500

    Initial program skeleton

---
+++ RSA.py
@@ -0,0 +1,11 @@
+# Generate k-bit RSA public/private key pair
+def generate_key_pair(keysize):
+    pass
+
+# Encrypt binary data using public key
+def encrypt(data, public_key):
+    pass
+
+# Decrypt using private key
+def decrypt():
+    pass
